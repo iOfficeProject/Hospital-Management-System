e7adf98fddd8ad9b80df24673c97450f6a2896d4
Juli Sanjay Khobragade <julik@cybage.com>
2023-07-05 15:20:33 +0530

    Added CRUD operations for Roles

--- Hospital_Appointment_Booking_System/Interfaces/IRoleRepository.cs
+++ Hospital_Appointment_Booking_System/Interfaces/IRoleRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Hospital_Appointment_Booking_System.Interfaces
+﻿using Hospital_Appointment_Booking_System.Models;
+
+namespace Hospital_Appointment_Booking_System.Interfaces
 {
     public interface IRoleRepository
     {
+        Task AddRole(Role role);
+        Task DeleteRole(int roleId);
+        Task<List<Role>> GetAllRoles();
+        Task<Role> GetRoleById(int roleId);
     }
 }
