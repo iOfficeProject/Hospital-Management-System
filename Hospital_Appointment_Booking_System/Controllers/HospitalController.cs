c1a7c199fa8cc6c65f9b82a89dd09f815ec1abdb
Juned Jahidalli Mulla <junedmu@cybage.com>
2023-08-16 16:02:33 +0530

    Added Test Cases for Hospital Controller

--- Hospital_Appointment_Booking_System/Controllers/HospitalController.cs
+++ Hospital_Appointment_Booking_System/Controllers/HospitalController.cs
@@ -1,4 +1,9 @@
-﻿using Hospital_Appointment_Booking_System.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Hospital_Appointment_Booking_System.DTO;
+using Hospital_Appointment_Booking_System.Interfaces;
 using Hospital_Appointment_Booking_System.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -12,14 +17,16 @@ namespace Hospital_Appointment_Booking_System.Controllers
     public class HospitalController : ControllerBase
     {
         private readonly IHospitalRepository _hospitalRepository;
+        private readonly IMapper _mapper;
 
-        public HospitalController(IHospitalRepository hospitalRepository)
+        public HospitalController(IHospitalRepository hospitalRepository, IMapper mapper)
         {
             _hospitalRepository = hospitalRepository;
+            _mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Hospital>>> GetHospitals()
+        public async Task<IActionResult> GetHospitals()
         {
             try
             {
@@ -33,7 +40,7 @@ namespace Hospital_Appointment_Booking_System.Controllers
         }
 
         [HttpGet("{hospitalId}")]
-        public async Task<ActionResult<Hospital>> GetHospital(int hospitalId)
+        public async Task<IActionResult> GetHospital(int hospitalId)
         {
             try
             {
@@ -51,28 +58,29 @@ namespace Hospital_Appointment_Booking_System.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<Hospital>> CreateHospital(Hospital hospital)
+        public async Task<IActionResult> CreateHospital(HospitalDTO hospitalDto)
         {
             try
-            {            
-                bool hospitalCreated = await _hospitalRepository.AddHospital(hospital);
+            {
+                bool hospitalCreated = await _hospitalRepository.AddHospital(hospitalDto);
                 if (!hospitalCreated)
                 {
-                    return Conflict("Hospital name already exists."); 
+                    return Conflict("Hospital name already exists.");
                 }
-                return Ok(hospital);
+                return Ok();
             }
             catch (Exception)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while creating the hospital.");
             }
-        }      
+        }
+
         [HttpPut("{hospitalId}")]
-        public async Task<ActionResult<Hospital>> UpdateHospital(int hospitalId, Hospital hospital)
+        public async Task<ActionResult<HospitalDTO>> UpdateHospital(int hospitalId, HospitalDTO hospitalDto)
         {
             try
             {
-                var isHospitalUpdated = await _hospitalRepository.UpdateHospital(hospitalId, hospital);
+                var isHospitalUpdated = await _hospitalRepository.UpdateHospital(hospitalId, hospitalDto);
                 if (!isHospitalUpdated)
                 {
                     return Conflict("Hospital name already exists.");
@@ -89,7 +97,6 @@ namespace Hospital_Appointment_Booking_System.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while updating the hospital.");
             }
         }
-
 
         [HttpDelete("{hospitalId}")]
         public async Task<IActionResult> DeleteHospital(int hospitalId)
---
+++ Hospital_Appointment_Booking_System/Unit Tests/HospitalControllerTests.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using FakeItEasy;
+using Hospital_Appointment_Booking_System.Controllers;
+using Hospital_Appointment_Booking_System.DTO;
+using Hospital_Appointment_Booking_System.Interfaces;
+using Hospital_Appointment_Booking_System.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Hospital_Appointment_Booking_System.UnitTests
+{
+    public class HospitalControllerTests
+    {
+        private readonly IHospitalRepository _fakeHospitalRepository;
+        private readonly IMapper _fakeMapper;
+        private readonly HospitalController _controller;
+
+        public HospitalControllerTests()
+        {
+            _fakeHospitalRepository = A.Fake<IHospitalRepository>();
+            _fakeMapper = A.Fake<IMapper>();
+
+            _controller = new HospitalController(_fakeHospitalRepository, _fakeMapper);
+        }
+
+        [Fact]
+        public async Task GetHospitals_ReturnsOkResultWithHospitals()
+        {
+            // Arrange
+            var expectedHospitals = new List<HospitalDTO>
+            {
+                new HospitalDTO { HospitalName = "Ruby", Location = "Pune" },
+                new HospitalDTO { HospitalName = "Metro", Location = "Kolhapur" }
+            };
+
+            A.CallTo(() => _fakeHospitalRepository.GetAllHospital()).Returns(expectedHospitals);
+
+            // Act
+            var result = await _controller.GetHospitals();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, okResult.StatusCode);
+
+            var actualHospitals = Assert.IsType<List<HospitalDTO>>(okResult.Value);
+            Assert.Equal(expectedHospitals.Count, actualHospitals.Count);
+            Assert.Equal(expectedHospitals[0].HospitalName, actualHospitals[0].HospitalName);
+            Assert.Equal(expectedHospitals[1].Location, actualHospitals[1].Location);
+        }
+
+        [Fact]
+        public async Task GetAllHospitals_ReturnsEmptyResult()
+        {
+            // Arrange
+            A.CallTo(() => _fakeHospitalRepository.GetAllHospital()).Returns(new List<HospitalDTO>());
+
+            // Act
+            var result = await _controller.GetHospitals();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+
+            var actualHospitals = Assert.IsType<List<HospitalDTO>>(okResult.Value);
+            Assert.Empty(actualHospitals);
+        }
+
+        [Fact]
+        public async Task GetHospitalById_ReturnsHospitalDTO()
+        {
+            // Arrange
+            int hospitalId = 1;
+            var expectedHospital = new HospitalDTO { HospitalId = hospitalId, HospitalName = "Ruby", Location="Pune" };
+            A.CallTo(() => _fakeHospitalRepository.GetByIdHospital(hospitalId)).Returns(expectedHospital);
+
+            // Act
+            var result = await _controller.GetHospital(hospitalId);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedHospitals = Assert.IsType<HospitalDTO>(okResult.Value);
+            Assert.Equal(expectedHospital.HospitalId, returnedHospitals.HospitalId);
+            Assert.Equal(expectedHospital.HospitalName, returnedHospitals.HospitalName);
+            Assert.Equal(expectedHospital.Location, returnedHospitals.Location);
+
+        }
+
+        [Fact]
+        public async Task CreateHospital_WithValidDto_ReturnsOkResult()
+        {
+            // Arrange
+            var hospitalDto = new HospitalDTO { HospitalName = "Metro Hospital",Location="Pune"};
+            A.CallTo(() => _fakeHospitalRepository.AddHospital(hospitalDto)).Returns(true);
+
+            // Act
+            var result = await _controller.CreateHospital(hospitalDto);
+
+            // Assert
+            var okResult = Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public async Task CreateHospital_WithExistingName_ReturnsConflictResult()
+        {
+            // Arrange
+            var hospitalDto = new HospitalDTO { HospitalName = "Metro Hospital", Location = "Pune" };
+
+            A.CallTo(() => _fakeHospitalRepository.AddHospital(hospitalDto)).Returns(false);
+
+            // Act
+            var result = await _controller.CreateHospital(hospitalDto);
+
+            // Assert
+            var conflictResult = Assert.IsType<ConflictObjectResult>(result);
+            Assert.Equal(StatusCodes.Status409Conflict, conflictResult.StatusCode);
+            Assert.Equal("Hospital name already exists.", conflictResult.Value);
+        }
+
+        [Fact]
+        public async Task UpdateHospital_WithValidData_ReturnsOkResultWithUpdatedHospital()
+        {
+            // Arrange
+            int fakeHospitalId = 1;
+            var fakeHospitalDTO = new HospitalDTO { HospitalName = "Updated Ruby", Location = "Updated Pune" };
+
+            A.CallTo(() => _fakeHospitalRepository.UpdateHospital(fakeHospitalId, fakeHospitalDTO))
+                .Returns(true);
+
+            A.CallTo(() => _fakeHospitalRepository.GetByIdHospital(fakeHospitalId))
+                .Returns(fakeHospitalDTO);
+
+            // Act
+            var result = await _controller.UpdateHospital(fakeHospitalId, fakeHospitalDTO);
+
+            // Assert
+            Assert.IsType<ActionResult<HospitalDTO>>(result);
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+
+            var updatedHospitalDTO = Assert.IsType<HospitalDTO>(okResult.Value);
+            Assert.Equal(fakeHospitalDTO.HospitalName, updatedHospitalDTO.HospitalName);
+            Assert.Equal(fakeHospitalDTO.Location, updatedHospitalDTO.Location);
+        }
+
+
+
+        [Fact]
+        public async Task DeleteSpecialization_ReturnsOkResult()
+        {
+            // Arrange
+            int hospitalId = 1;
+            A.CallTo(() => _fakeHospitalRepository.DeleteHospital(hospitalId));
+
+            // Act
+            var result = await _controller.DeleteHospital(hospitalId);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, okResult.StatusCode);
+            Assert.Equal("Hospital is deleted.", okResult.Value);
+
+        }
+
+    }
+}
